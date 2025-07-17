9eab4098dd1a230549c7faae637a38ec24f01896
nicklamiller <nicklamiller@gmail.com>
2025-07-16 22:10:57 -0700

    Week 2 Fact Data Modeling Lab2 SQL query

---
+++ bootcamp/materials/2-fact-data-modeling/sql/personal_queries/labs/lab2.sql
@@ -0,0 +1,72 @@
+-- -- DDL
+-- DROP TABLE IF EXISTS users_cumulated;
+-- CREATE TABLE users_cumulated (
+-- 	user_id TEXT,
+-- 	dates_active DATE[],
+-- 	date DATE,
+-- 	PRIMARY KEY(user_id, date)
+-- );
+
+
+WITH yesterday AS (
+	SELECT *
+	FROM users_cumulated
+	WHERE DATE(CAST(date AS TIMESTAMP)) = DATE('2023-01-30')
+)
+, today AS (
+	SELECT
+		CAST(user_id AS TEXT) AS user_id,
+		DATE(CAST(event_time AS TIMESTAMP)) AS date_active
+	FROM events
+	WHERE DATE(CAST(event_time AS TIMESTAMP)) = DATE('2023-01-31')
+		AND user_id IS NOT NULL
+	GROUP BY user_id, DATE(CAST(event_time AS TIMESTAMP))
+)
+
+INSERT INTO users_cumulated
+SELECT
+	COALESCE(t.user_id, y.user_id) AS user_id,
+	CASE
+		WHEN y.dates_active IS NULL THEN ARRAY[t.date_active]
+		WHEN t.date_active IS NULL THEN y.dates_active
+		ELSE ARRAY[t.date_active] || y.dates_active
+	END AS dates_active,
+	COALESCE(t.date_active, y.date + INTERVAL '1 day') AS date
+FROM today t
+FULL OUTER JOIN yesterday y USING(user_id);
+
+SELECT *
+FROM users_cumulated
+ORDER BY date DESC;
+
+WITH users AS (
+	SELECT * FROM users_cumulated
+	WHERE date = DATE('2023-01-31')
+)
+, series AS (
+	SELECT *
+	FROM generate_series(
+		DATE('2023-01-01'), DATE('2023-01-31'), INTERVAL '1 day'
+	) AS series_date
+)
+, placeholder_ints AS (
+	SELECT
+		CASE
+			WHEN dates_active @> ARRAY [DATE(series_date)]
+				THEN CAST(POW(2, 32 - (date - DATE(series_date))) AS BIGINT)
+			ELSE 0
+		END AS placeholder_int_value,
+		*
+	FROM users CROSS JOIN series
+)
+
+SELECT
+	user_id,
+	CAST(CAST(SUM(placeholder_int_value) AS BIGINT) AS BIT(32)),
+	BIT_COUNT(CAST(CAST(SUM(placeholder_int_value) AS BIGINT) AS BIT(32))) AS dim_is_monthly_active,
+	BIT_COUNT(CAST('11111110000000000000000000000000' AS BIT(32)) &
+		CAST(CAST(SUM(placeholder_int_value) AS BIGINT) AS BIT(32))) > 0 AS dim_is_weekly_active,
+	BIT_COUNT(CAST('10000000000000000000000000000000' AS BIT(32)) &
+		CAST(CAST(SUM(placeholder_int_value) AS BIGINT) AS BIT(32))) > 0 AS dim_is_daily_active
+FROM placeholder_ints
+GROUP BY user_id;
